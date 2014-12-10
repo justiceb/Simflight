269bd3e4fef6f98c8a2d1852bf78f746329a2f0f
justiceb <justiceb@purdue.edu>
2014-12-10 11:23:06 -0500

    remove all files unrelated to the flightsim

--- flatearth/flying_qualities/deg2rad.m
+++
@@ -1,7 +0,0 @@
-function [ output ] = deg2rad( input )
-%DEG2RAD Summary of this function goes here
-%   Detailed explanation goes here
-output = input * 0.0174532925;
-
-end
-
--- flatearth/flying_qualities/rad2deg.m
+++
@@ -1,7 +0,0 @@
-function [ output ] = rad2deg( input )
-%RAD2DEG Summary of this function goes here
-%   Detailed explanation goes here
-output = input/0.0174532925;
-
-end
-
